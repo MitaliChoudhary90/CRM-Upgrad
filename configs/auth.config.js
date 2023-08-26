c3f660e5fca3d787e0ebdace8286eb2d8cedc0e1
MITALI CHOUDHARY <90467167+MitaliChoudhary90@users.noreply.github.com>
2023-08-26 16:15:31 +0530

    Delete configs directory

--- configs/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-    secret:"My personal secret"  //this is the secret key for token generation
-}
--- configs/server.config.js
+++
@@ -1,5 +0,0 @@
-/** Whatever we write in the module.expose is exposed to the other file in the project */
-
-module.exports={
-    PORT: 8081
-}
